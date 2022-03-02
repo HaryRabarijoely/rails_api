a930fbd64bae42c1d9bbb1f18ed63fad19e36c31
hary rabarijoely <rabonia@hotmail.com>
2022-03-02 01:38:35 +0100

    adding relation user-articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,26 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show update destroy ]
+  before_action :set_article, only: [:show, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /articles
   def index
-    @articles = Article.all
+    @articles = Article.public_articles
 
     render json: @articles
   end
 
   # GET /articles/1
   def show
-    render json: @article
+    if @article.private_article == false || (current_user && current_user.id == @article.user_id)
+      render json: @article
+    else
+      render json: { message: "This article is private."}, status: :unauthorized
+    end
   end
 
   # POST /articles
   def create
-    @article = Article.new(article_params)
+    @article = Article.new(title: params[:title], content:params[:content], user_id:current_user.id, private:params[:private])
 
     if @article.save
       render json: @article, status: :created, location: @article
@@ -26,16 +31,24 @@ class ArticlesController < ApplicationController
 
   # PATCH/PUT /articles/1
   def update
-    if @article.update(article_params)
-      render json: @article
+    if @article.user_id == current_user.id
+      if @article.update(title: params[:title], content:params[:content], private:params[:private])
+        render json: @article
+      else
+        render json: @article.errors, status: :unprocessable_entity
+      end
     else
-      render json: @article.errors, status: :unprocessable_entity
+      render json: { message: "You cannot modify other authors' articles."}, status: :unauthorized
     end
   end
 
   # DELETE /articles/1
   def destroy
-    @article.destroy
+    if @article.user_id == current_user.id
+      @article.destroy
+    else
+      render json: { message: "You cannot delete other authors' articles."}, status: :unauthorized
+    end
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,30 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 
+Article.destroy_all
+User.destroy_all
+
+3.times do
+  User.create!(    
+    email:Faker::Name.first_name + "@yopmail.com",
+    password:'azerty'
+  )
+end
+
 10.times do
   Article.create!(
     title:Faker::Restaurant.name,
-    content:Faker::Restaurant.description    
+    content:Faker::Restaurant.description,
+    user_id: User.all.sample.id,
+    private_article:false
+  )
+end
+
+3.times do
+  Article.create!(
+    title:Faker::Restaurant.name,
+    content:Faker::Restaurant.description,
+    user_id: User.all.sample.id,
+    private_article:true
   )
 end
